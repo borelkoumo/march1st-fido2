51b62730a74101dddcd2080584dbc645a0d8dcd0
Borel KOUMO <borelkoumo@yahoo.fr>
2021-12-29 22:00:46 +0100

    Initial commit

---
+++ server/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+-- test rp
+insert into rp (id, name, description) values('app.march1st-beta.com', 'March 1st', 'March1st application');
+insert into rp (id, name, description) values('localhost', 'March 1st - Localhost', 'March1st localhost application');
+insert into rp (id, name, description) values('dexsp2mqnjk70.amplifyapp.com', 'March 1st - Localhost', 'March1st application');
