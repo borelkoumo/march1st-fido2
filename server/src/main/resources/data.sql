1add0626c0935829da516ddced4784fbdeb02b2c
Borel KOUMO <borelkoumo@yahoo.fr>
2021-12-31 15:54:40 +0100

    Cleaned unused files

--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -1,4 +1,3 @@
 -- test rp
-insert into rp (id, name, description) values('app.march1st-beta.com', 'March 1st', 'March1st application');
 insert into rp (id, name, description) values('localhost', 'March 1st - Localhost', 'March1st localhost application');
-insert into rp (id, name, description) values('dexsp2mqnjk70.amplifyapp.com', 'March 1st - Localhost', 'March1st application');
+insert into rp (id, name, description) values('march1st.com', 'March 1st', 'March 1st application');
