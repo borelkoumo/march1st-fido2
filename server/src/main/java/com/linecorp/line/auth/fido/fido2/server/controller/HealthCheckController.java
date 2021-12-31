d755e2ae1a8b93765b6c1e083424c54d78569435
Borel KOUMO <borelkoumo@yahoo.fr>
2021-12-31 16:18:49 +0100

    Added prefix fido2 to healthcheck controller

--- server/src/main/java/com/linecorp/line/auth/fido/fido2/server/controller/HealthCheckController.java
+++ server/src/main/java/com/linecorp/line/auth/fido/fido2/server/controller/HealthCheckController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthCheckController {
-    @GetMapping(path = "/health")
+    @GetMapping(path = "fido2/health")
     public String healthCheck() {
         return "OK";
     }
